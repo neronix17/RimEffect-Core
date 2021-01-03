985c438d99c61ea5120fbddf1241c11b45aad9ad
neronix17 <39186138+neronix17@users.noreply.github.com>
2021-01-03 21:05:30 +0000

    Adjusted Medi-Gel
    - Medi-gel recipe adjusted so workbench is RE_ManufacturingBench
    - Medi-gel now works as normal medicine, behaving like it should when used or administered through surgery.

---
+++ Source/Neronix17/RECompMediGel/HarmonyPatches.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+using HarmonyLib;
+
+namespace RECompMediGel
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            Harmony harmony = new Harmony("com.neronix17.rimeffect.medigel");
+
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(TendUtility), "DoTend")]
+    static class TendUtility_DoTend
+    {
+        [HarmonyPrefix]
+        static bool Prefix(Pawn doctor, Pawn patient, Medicine medicine)
+        {
+            if (medicine != null && medicine.def.defName == "RE_MediGel")
+            {
+                MediGelUtility.TrySealWounds(patient);
+            }
+            return true;
+        }
+
+        [HarmonyPostfix]
+        static void Postfix(Pawn doctor, Pawn patient, Medicine medicine)
+        {
+        }
+    }
+}
