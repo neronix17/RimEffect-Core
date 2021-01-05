90263d160c4f094e4136660f1092b9176a1a8d0e
Taranchuk <nfhfyxer@gmail.com>
2021-01-05 11:19:03 +0200

    Ammo Belt code

---
+++ Source/RimEffect/Damages/DamageWorker_BombNoShake.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimEffect
+{
+	internal class DamageWorker_BombNoShake : DamageWorker_AddInjury
+	{
+		public override void ExplosionStart(Explosion explosion, List<IntVec3> cellsToAffect)
+		{
+			if (def.explosionHeatEnergyPerCell > float.Epsilon)
+			{
+				GenTemperature.PushHeat(explosion.Position, explosion.Map, def.explosionHeatEnergyPerCell * (float)cellsToAffect.Count);
+			}
+			MoteMaker.MakeStaticMote(explosion.Position, explosion.Map, ThingDefOf.Mote_ExplosionFlash, explosion.radius * 6f);
+			ExplosionVisualEffectCenter(explosion);
+		}
+	}
+}
