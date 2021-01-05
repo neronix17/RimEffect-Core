af83d30773f5f714c7e6cd4a8c9ac96321e25823
Taranchuk <nfhfyxer@gmail.com>
2021-01-05 12:22:35 +0200

    added new toggle for ammo belt

---
+++ Source/RimEffect/Textures.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RimEffect
+{
+    [StaticConstructorOnStartup]
+    public static class Textures
+    {
+        public static Texture2D AmmoBeltToggle = ContentFinder<Texture2D>.Get("UI/AmmoBeltToggle");
+    }
+}
