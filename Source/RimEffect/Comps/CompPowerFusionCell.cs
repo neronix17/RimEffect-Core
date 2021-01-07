1013621ecf133352945433898dfd397728f4d14d
Taranchuk <nfhfyxer@gmail.com>
2021-01-07 15:07:06 +0200

    new power comps

---
+++ Source/RimEffect/Comps/CompPowerFusionCell.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimEffect
+{
+	internal class CompPowerFusionCell : CompPowerPlant
+	{
+        public override void PostExposeData()
+        {
+			Thing refee = null;
+			if (Scribe.mode == LoadSaveMode.Saving && connectParent != null)
+			{
+				refee = connectParent.parent;
+			}
+			Scribe_References.Look(ref refee, "parentThingFusionCell");
+			if (refee != null)
+			{
+				connectParent = ((ThingWithComps)refee).GetComp<CompPower>();
+			}
+			if (Scribe.mode == LoadSaveMode.PostLoadInit && connectParent != null)
+			{
+				ConnectToTransmitter(connectParent, reconnectingAfterLoading: true);
+			}
+		}
+    }
+}
