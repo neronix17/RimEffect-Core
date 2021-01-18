6ab7fc6621238a8f7092ac173b0882ccdf968458
Simon Latusek <simon.latusek@gmail.com>
2021-01-18 18:22:23 +0100

    biotic pull

--- Source/RimEffect/Abilities/Abilities/Ability_GiveHediff.cs
+++ Source/RimEffect/Abilities/Abilities/Ability_GiveHediff.cs
@@ -2,6 +2,8 @@
 
 namespace RimEffect
 {
+    using HarmonyLib;
+    using RimWorld;
     using Verse;
 
     public class Ability_GiveHediff : Ability
@@ -15,6 +17,36 @@ namespace RimEffect
             if(Math.Abs(extension.severity - -1f) > 0.01f)
                 hediff.Severity = extension.severity;
             target.Pawn.health.AddHediff(hediff);
+        }
+    }
+
+    public class Ability_GiveHediffAndPull : Ability_GiveHediff
+    {
+        public override void Cast(LocalTargetInfo target)
+        {
+            IntVec3 destination = this.pawn.Position + ((target.Cell - this.pawn.Position).ToVector3().normalized * 2).ToIntVec3();
+
+            /*
+            if (!parent.def.HasAreaOfEffect)
+            {
+                parent.AddEffecterToMaintain(EffecterDefOf.Skip_Entry.Spawn(target.Thing, pawn.Map), target.Thing.Position, 60);
+            }
+
+            if (Props.destination == AbilityEffectDestination.Selected)
+            {
+                parent.AddEffecterToMaintain(EffecterDefOf.Skip_Exit.Spawn(target.Cell, pawn.Map), destination, 60);
+            }
+            */
+            target.Thing.TryGetComp<CompCanBeDormant>()?.WakeUp();
+            target.Thing.Position = destination;
+
+            if (target.Thing is Pawn pawn2)
+            {
+                //pawn2.stances.stunner.StunFor_NewTmp(GenDate.TicksPerHour, pawn, addBattleLog: false, showMote: false);
+                pawn2.Notify_Teleported();
+            }
+
+            base.Cast(target);
         }
     }
 
