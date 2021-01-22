15c358e868670ce2cac48753e1f677f32796dc32
Taranchuk <nfhfyxer@gmail.com>
2021-01-22 03:39:19 +0200

    added Colony Growth quest

---
+++ Source/RimEffect/QuestNodes/QuestNode_GeneratePawns.cs
@@ -0,0 +1,41 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using RimWorld.QuestGen;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace RimEffect
+{
+	public class QuestNode_GeneratePawns : QuestNode
+	{
+		[NoTranslate]
+		public SlateRef<string> storeAs;
+
+		public SlateRef<int> count;
+
+		public SlateRef<List<PawnKindDef>> pawnKindDefs;
+		protected override bool TestRunInt(Slate slate)
+		{
+			return true;
+		}
+		protected override void RunInt()
+		{
+			List<Pawn> pawns = new List<Pawn>();
+			var slate = QuestGen.slate;
+			for (var i = 0; i < count.GetValue(slate); i++)
+            {
+				var pawn = PawnGenerator.GeneratePawn(pawnKindDefs.GetValue(slate).RandomElement());
+				pawns.Add(pawn);
+            }
+			slate.Set<List<Pawn>>(storeAs.GetValue(slate), pawns);
+			var personnelData = "";
+			foreach (var pawn in pawns)
+            {
+				personnelData += "\t" + pawn.LabelShort + ", " + pawn.kindDef.label + "\n";
+            }
+			slate.Set("personnelData", personnelData);
+		}
+	}
+}
---
+++ Source/RimEffect/QuestNodes/QuestNode_GetRoomsToBuild.cs
@@ -0,0 +1,83 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using RimWorld.QuestGen;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace RimEffect
+{
+	public class QuestNode_GetRoomsToBuild : QuestNode
+	{
+		[NoTranslate]
+		public SlateRef<string> storeAs;
+
+		public SlateRef<List<RoomRoleDef>> roomRoleDefs;
+		public SlateRef<int> count;
+		public SlateRef<int> minRoomSize;
+		protected override bool TestRunInt(Slate slate)
+		{
+			return true;
+		}
+
+		protected override void RunInt()
+		{
+			Dictionary<RoomRoleDef, int> roomsDict = new Dictionary<RoomRoleDef, int>();
+			List<RoomRoleDef> rooms = new List<RoomRoleDef>();
+			var slate = QuestGen.slate;
+			for (var i = 0; i < count.GetValue(slate); i++)
+			{
+				if (roomRoleDefs.GetValue(slate).TryRandomElement(out RoomRoleDef result))
+				{
+					if (roomsDict.ContainsKey(result))
+					{
+						roomsDict[result]++;
+					}
+					else
+					{
+						roomsDict[result] = 1;
+					}
+					rooms.Add(result);
+				}
+			}
+
+			if (rooms.Any())
+			{
+				slate.Set(storeAs.GetValue(slate), rooms);
+				string roomsToBeBuild = "";
+				foreach (var room in roomsDict)
+				{
+					roomsToBeBuild += "\t" + room.Value + "x " + room.Key.label + "\n";
+				}
+				slate.Set("roomsToBeBuild", roomsToBeBuild);
+				slate.Set(storeAs.GetValue(slate), rooms);
+				var map = slate.Get<Map>("map");
+				QuestPart_RoomRequirement questPart_RoomRequirement = new QuestPart_RoomRequirement();
+				questPart_RoomRequirement.inSignal = QuestGenUtility.HardcodedSignalWithQuestID("RoomConstructionCheck");
+				questPart_RoomRequirement.map = map;
+				var oldRooms = new List<RoomRequirement>();
+				foreach (var room in map.regionGrid.allRooms)
+                {
+					var roomReq = new RoomRequirement();
+					roomReq.currentCellCount = room.CellCount;
+					roomReq.roomRoleDef = room.Role;
+					oldRooms.Add(roomReq);
+				}
+				questPart_RoomRequirement.oldRooms = oldRooms;
+				var newRooms = new List<RoomRequirement>();
+				var minSize = minRoomSize.GetValue(slate);
+				foreach (var room in rooms)
+                {
+					var roomReq = new RoomRequirement();
+					roomReq.minimumCellRequirement = minSize * minSize;
+					roomReq.roomRoleDef = room;
+					newRooms.Add(roomReq);
+				}
+				questPart_RoomRequirement.roomsToBeBuilt = newRooms;
+				slate.Set<string>("minRoomSizeInfo", minSize + "x" + minSize);
+				QuestGen.quest.AddPart(questPart_RoomRequirement);
+			}
+		}
+	}
+}
