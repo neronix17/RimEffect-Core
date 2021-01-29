2f357c4c88a66a896490e74df2dd8a4d3599fa6c
Simon Latusek <simon.latusek@gmail.com>
2021-01-29 21:40:02 +0100

    Incinerate + Overload + Sabotage Ability

---
+++ Source/RimEffect/Abilities/Abilities/Ability_Sabotage.cs
@@ -0,0 +1,22 @@
+﻿namespace RimEffect
+{
+    using RimWorld;
+    using UnityEngine;
+    using Verse;
+
+    public class Ability_Sabotage : Ability
+    {
+        public override void Cast(LocalTargetInfo target)
+        {
+            base.Cast(target);
+
+            if (target.Pawn != null)
+            {
+                if (target.Pawn.RaceProps.IsFlesh)
+                    target.Pawn.stances.StaggerFor(Mathf.RoundToInt(this.GetPowerForPawn() * GenTicks.TicksPerRealSecond));
+                else if (target.Pawn.RaceProps.IsMechanoid)
+                    target.Pawn.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.Berserk, null, forceWake: true);
+            }
+        }
+    }
+}
