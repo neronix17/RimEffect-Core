e95c451d600296b01614c2559f2a9730efa2c2aa
Simon Latusek <simon.latusek@gmail.com>
2021-02-21 13:50:03 +0100

    aborting casting doesn't cause instant cast anymore

--- Source/RimEffect/Abilities/JobDriver_CastAbilityOnce.cs
+++ Source/RimEffect/Abilities/JobDriver_CastAbilityOnce.cs
@@ -16,15 +16,21 @@
 
             //yield return Toils_Combat.GotoCastPosition(TargetIndex.A);
 
-            CompAbilities comp = this.pawn.GetComp<CompAbilities>();
-            Toil          toil = Toils_General.Wait(comp.currentlyCasting.GetCastTimeForPawn(), TargetIndex.A);
-            toil.WithProgressBarToilDelay(TargetIndex.C);
-            toil.AddFinishAction(newAct: () =>
-                                         {
-                                             LocalTargetInfo target        = this.pawn.jobs.curJob.GetTarget(TargetIndex.A);
-                                             comp.currentlyCasting.Cast(target);
-                                         });
-            yield return toil;
+            CompAbilities comp     = this.pawn.GetComp<CompAbilities>();
+            Toil          waitToil = Toils_General.Wait(comp.currentlyCasting.GetCastTimeForPawn(), TargetIndex.A);
+            waitToil.WithProgressBarToilDelay(TargetIndex.C);
+            yield return waitToil;
+
+            Toil castToil = new Toil();
+            castToil.initAction = () =>
+                                  {
+                                      LocalTargetInfo target = castToil.actor.jobs.curJob.GetTarget(TargetIndex.A);
+                                      comp.currentlyCasting.Cast(target);
+                                  };
+            castToil.defaultCompleteMode = ToilCompleteMode.Instant;
+            castToil.atomicWithPrevious  = true;
+            yield return castToil;
+
         }
     }
 }
