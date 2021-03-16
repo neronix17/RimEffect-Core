bde4c9e07e82ce6e4735b92d690a7ae8eda895b4
Taranchuk <nfhfyxer@gmail.com>
2021-03-16 21:28:13 +0200

    added new stat part for MarketValue

---
+++ Source/RimEffect/Stats/StatPart_ModifyMarketValue.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+
+namespace RimEffect
+{
+	public class StatPart_ModifyMarketValue : StatPart
+	{
+		public override string ExplanationPart(StatRequest req)
+		{
+			if (req.HasThing)
+			{
+
+			}
+			return null;
+		}
+
+		public override void TransformValue(StatRequest req, ref float val)
+		{
+			if (req.HasThing && req.Thing.ParentHolder is Pawn_InventoryTracker inventoryTracker 
+				&& inventoryTracker.pawn.GetLord()?.LordJob is LordJob_TradeWithColony lordJob && lordJob.lord.faction?.def == RE_DefOf.RE_SystemsAlliance)
+			{
+				val *= 0.75f;
+			}
+		}
+	}
+}
+ 
