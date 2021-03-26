2167a6f7a3a7bf60939715e84b1959cc312d5a2b
neronix17 <39186138+neronix17@users.noreply.github.com>
2021-03-26 15:02:53 +0000

    Medi-Gel Fixes
    - Added checks for chronic/sickness so they don't get removed.

--- Source/Neronix17/RECompMediGel/MediGelUtility.cs
+++ Source/Neronix17/RECompMediGel/MediGelUtility.cs
@@ -22,26 +22,12 @@ namespace RECompMediGel
                 List<Hediff> list = enumerable.ToList();
                 for (int i = 0; i < list.Count; i++)
                 {
-                    if (list[i] != null)
+                    if (list[i] != null && !list[i].def.makesSickThought && !list[i].def.chronic)
                     {
                         if (list[i].def.hediffClass == typeof(Hediff_MissingPart))
                         {
                             list[i].Tended_NewTemp(1.0f, 1.0f);
                         }
-                        //else if(list[i].def.tendable)
-                        //{
-                        //    HediffWithComps hediffWithComps = list[i] as HediffWithComps;
-                        //    if (hediffWithComps != null)
-                        //    {
-                        //        HediffComp_TendDuration hediffComp_TendDuration = HediffUtility.TryGetComp<HediffComp_TendDuration>(hediffWithComps);
-                        //        if (hediffComp_TendDuration != null)
-                        //        {
-                        //            hediffComp_TendDuration.tendQuality = 1.0f;
-                        //            hediffComp_TendDuration.tendTicksLeft = Find.TickManager.TicksGame;
-                        //        }
-                        //        pawn.health.Notify_HediffChanged(list[i]);
-                        //    }
-                        //}
                         else if (!list[i].IsPermanent() && list[i].def.everCurableByItem)
                         {
                             HealthUtility.CureHediff(list[i]);
