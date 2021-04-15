7881ac225682f2bc2bacf5ca097748f1fd8a4ef0
Taranchuk <nfhfyxer@gmail.com>
2021-04-15 15:38:35 +0300

    update

--- Source/RimEffect/Apparels/SpectreArmor.cs
+++ Source/RimEffect/Apparels/SpectreArmor.cs
@@ -7,11 +7,24 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using Verse;
+using VFEMech;
 
 namespace RimEffect
 {
     public class SpectreArmor : Apparel
     {
+        public override bool CheckPreAbsorbDamage(DamageInfo dinfo)
+        {
+            var shield = this.GetComp<ShieldMechBubble>();
+            if (shield != null)
+            {
+                return shield.AbsorbingDamage(dinfo, out bool absorbed);
+            }
+            else
+            {
+                return base.CheckPreAbsorbDamage(dinfo);
+            }
+        }
         public override void DrawWornExtras()
         {
             base.DrawWornExtras();
