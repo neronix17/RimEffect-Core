72a30a38a8534e19d255e5c5c14a0c9e0463bd3e
Taranchuk <nfhfyxer@gmail.com>
2021-05-12 18:17:00 +0300

    update

--- Source/RimEffect/Stats/StatPart_ModifyMarketValue.cs
+++ Source/RimEffect/Stats/StatPart_ModifyMarketValue.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using RimWorld.Planet;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,21 +16,38 @@ namespace RimEffect
 	{
 		public override string ExplanationPart(StatRequest req)
 		{
-			if (req.HasThing)
-			{
-
-			}
-			return null;
+			return "RE.AllyRelationsDiscount".Translate();
 		}
 
 		public override void TransformValue(StatRequest req, ref float val)
 		{
-			if (req.HasThing && req.Thing.ParentHolder is Pawn_InventoryTracker inventoryTracker 
-				&& inventoryTracker.pawn.GetLord()?.LordJob is LordJob_TradeWithColony lordJob 
-				&& lordJob.lord.faction?.def == RE_DefOf.RE_SystemsAlliance && lordJob.lord.faction.RelationKindWith(Faction.OfPlayer) == FactionRelationKind.Ally)
-			{
-				val *= 0.75f;
+			if (req.HasThing)
+            {
+				if (req.Thing.ParentHolder is Pawn_InventoryTracker inventoryTracker && inventoryTracker.pawn.GetLord()?.LordJob is LordJob_TradeWithColony lordJob 
+					&& FactionHasDiscount(lordJob.lord.faction))
+                {
+					Log.Message("Removing");
+					val *= 0.75f;
+				}
+				else if (req.Thing.ParentHolder is Settlement_TraderTracker settlement_TraderTracker && FactionHasDiscount(settlement_TraderTracker.settlement.Faction))
+                {
+					Log.Message("Removing");
+					val *= 0.75f;
+				}
+				else if (req.Thing.ParentHolder is Caravan_TraderTracker caravan_TraderTracker)
+				{
+					var caravan = Traverse.Create(caravan_TraderTracker).Field("caravan").GetValue<Caravan>();
+					if (FactionHasDiscount(caravan.Faction))
+                    {
+						Log.Message("Removing");
+						val *= 0.75f;
+                    }
+				}
 			}
+		}
+		private bool FactionHasDiscount(Faction faction)
+        {
+			return faction?.def == RE_DefOf.RE_SystemsAlliance && faction.RelationKindWith(Faction.OfPlayer) == FactionRelationKind.Ally;
 		}
 	}
 }
